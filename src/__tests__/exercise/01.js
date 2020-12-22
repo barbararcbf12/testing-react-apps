e52e67689e18b76353e7fbdf9e1005683e838200
Barbara Ferreira <bbf@charlietango.dk>
2020-12-22 22:03:46 +0100

    Ex.1 - EC1 - Solution

--- src/__tests__/exercise/01.js
+++ src/__tests__/exercise/01.js
@@ -24,11 +24,25 @@ test('counter increments and decrements when the buttons are clicked', () => {
   // 🐨 expect the message.textContent toBe 'Current count: 0'
   expect(message.textContent).toBe('Current count: 0')
   // 🐨 click the increment button (💰 increment.click())
-  buttons[1].click()
+  // buttons[1].click()
+  buttons[1].dispatchEvent(
+    new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true,
+      button: 0,
+    }),
+  )
   // 🐨 assert the message.textContent
   expect(message.textContent).toBe('Current count: 1')
   // 🐨 click the decrement button (💰 decrement.click())
-  buttons[0].click()
+  // buttons[0].click()
+  buttons[0].dispatchEvent(
+    new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true,
+      button: 0,
+    }),
+  )
   // 🐨 assert the message.textContent
   expect(message.textContent).toBe('Current count: 0')
   // 🐨 cleanup by removing the div from the page (💰 div.remove())
